decd52bb4feec654484ad1f3a1547419a4f9b2a5
Erick.S <esatire@gmail.com>
2017-12-07 10:48:38 +0200

    Fixed typo, test commit.

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router';
 export default () => {
   return <div class="boxed-view">
     <div class="boxed-view__box">
-      <h1>Page Not Found</h1>
+      <h1>404 - Page Not Found</h1>
       <p>Hmmm, we're unable to find that page.</p>
       <Link to="/" className="button button--link">Head Home</Link>
     </div>
